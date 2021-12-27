cb0976d5d164efe58835c1f5f1bad997de6709fa
Alessandro Resta <alessandro.resta@gmail.com>
2021-12-27 17:49:16 +0100

    Add handler option for passing authz code back to the invoker

--- callbacksrv.go
+++ callbacksrv.go
@@ -8,13 +8,30 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type HandlerOption func(req *http.Request)
+
+func WithCodeChan(codeChan chan<- string) HandlerOption {
+	return func(req *http.Request) {
+		codeChan <- req.FormValue("code")
+		close(codeChan)
+	}
+}
+
 // Serve implements a http server prepared to serve a single callback request on demand.
 // When the callback request is received, the server will send a notification to the notifyChan channel,
 // close the channel sinalizing that no more requests will be served, and shuts down itself gracefully.
-func Serve(notifyChan chan struct{}, quitChan chan os.Signal) {
+func Serve(notifyChan chan struct{}, quitChan chan os.Signal, opts ...HandlerOption) {
 	router := gin.Default()
 
 	router.GET("/callback", func(c *gin.Context) {
+
+		for _, opt := range opts {
+			// TODO: clone the request before passing
+			// to the option it might empty the buffer.
+			opt(c.Request)
+		}
+
+		c.Writer.Header().Set("Content-Type", "text/plain")
 		c.String(http.StatusOK, "Authorization Granted. Bye!")
 
 		notifyChan <- struct{}{}
